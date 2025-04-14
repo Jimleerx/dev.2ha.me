ee74bf3f80403606e213c8622c306c57aab83920
Jimgo <li@2ha.me>
2025-04-14 16:52:15 +0800

    blog page add tags count

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -7,7 +7,7 @@ export async function parseAuthors(authors: string[]) {
       return {
         id,
         name: author?.data?.name || id,
-        avatar: author?.data?.avatar || '/static/logo.png',
+        avatar: author?.data?.avatar || '/static/logo.webp',
         isRegistered: !!author,
       }
     } catch (error) {
@@ -15,7 +15,7 @@ export async function parseAuthors(authors: string[]) {
       return {
         id,
         name: id,
-        avatar: '/static/logo.png',
+        avatar: '/static/logo.webp',
         isRegistered: false,
       }
     }
